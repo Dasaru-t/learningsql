5a27ae237c7c0a0fefee0e114e706711aef999c1
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2025-02-03 22:12:55 +0530

    Update Code Challenge State score metrics.sql

--- Code Challenge State score metrics.sql
+++ Code Challenge State score metrics.sql
@@ -1,6 +1,2 @@
-SELECT first_name, last_name, quiz_points
-FROM people
-WHERE quiz_points = 
-  (
-  SELECT MAX(quiz_points) FROM people
-  );
+SELECT first_name, SUBSTR(last_name,1,3)
+FROM people;
