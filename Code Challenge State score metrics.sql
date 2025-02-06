bf4a91dc2b5df4479b2b5ed8dd87859aab53bfc3
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2025-02-06 22:37:14 +0530

    Update Code Challenge State score metrics.sql

--- Code Challenge State score metrics.sql
+++ Code Challenge State score metrics.sql
@@ -1,2 +1,5 @@
-SELECT first_name, SUBSTR(last_name,1,3)
-FROM people;
+SELECT 
+    COUNT(*) AS TotalRowCount,
+    SUM(CASE WHEN [SuperLoad] IS NULL THEN 1 ELSE 0 END) AS NullCount
+FROM 
+    [dw].[009_FactAtRiskReportBase];
