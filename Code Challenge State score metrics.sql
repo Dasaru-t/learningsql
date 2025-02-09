e6f3d96265a5983e8f7102844ac4c96753fda671
Dasaru Thamaranga <63537226+Dasaru-t@users.noreply.github.com>
2025-02-09 22:18:28 +0530

    Update Code Challenge State score metrics.sql

--- Code Challenge State score metrics.sql
+++ Code Challenge State score metrics.sql
@@ -1,3 +1,4 @@
-SELECT first_name, last_name, quiz_points
-FROM people
+SELECT s.state_name, p.quiz_points AS MAXPOINTS, p.quiz_points AS AVGPOINTS
+FROM states AS s LEFT JOIN people AS p
+ON s.state_abbrev = p.state_code
 WHERE quiz_points = (SELECT MAX(quiz_points) FROM people);
